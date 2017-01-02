8f03ee9a2d372625fd6eda7f4bcbb3635f6df0e8
Evan <github@erosson.org>
2017-01-01 19:11:11 -0800

    flavor: long vs. short, also sigfigs

--- src/main.js
+++ src/main.js
@@ -1,76 +1,122 @@
 import suffixes from './suffixes.json'
 // import _ from 'lodash'
-// import single pieces of lodash instead of the whole thing, to keep compiled lib size small
-import _defaults from 'lodash/defaults'
 
+function validate(condition, message) {
+  if (!condition) {
+    throw new Error(message)
+  }
+  return condition
+}
+
+// Suffixes are a list - which index of the list do we want? 
+// _index(999) === 0
+// _index(1000) === 1
+// _index(1000000) === 2
 function _index(val) {
-  // http://stackoverflow.com/a/23416775/2782048
-  // oops, fails for e >= 20
-  //return Math.floor(((''+val).length-1) / 3)
-  return Math.max(0, Math.floor(Math.log10(val)/3))
-}
-function _suffix(val, suffixes) {
-  return suffixes[_index(val)]
+  // string length is faster but fails for length >= 20, where JS starts
+  // formatting with e
+  return Math.max(0, Math.floor(Math.log10(Math.abs(val))/3))
 }
 
-// All format choices, like on swarmsim's options screen. 
-const Formats = {
-  standard(val, opts) {
-    const index = _index(val)
-    if (val < opts.minSuffix) {
-      return Math.floor(val).toLocaleString()
-    }
-    const suffix = opts.suffixFn(index)
-    if (!suffix && suffix !== '') {
-      return val.toExponential(opts.sigfigs-1).replace('e+', 'e')
-    }
-    const prefix = (val / Math.pow(1000, index)).toPrecision(opts.sigfigs)
-    return `${prefix}${suffix}`
-  },
-  hybrid(val, opts) {
-    opts.suffixes = opts.suffixes.slice(0, 12)
-    return this.standard(val, opts)
-  },
-  scientific(val, opts) {
-    opts.suffixes = []
-    return this.standard(val, opts)
-  },
-  engineering(val, opts) {
-    opts.suffixFn = function(index) {
-      if (index > 0) {
-        return `E${index*3}`
-      }
-      return ''
-    }
-    return this.standard(val, opts)
-  },
+// The formatting function.
+function _format(val, opts) {
+  const index = _index(val)
+  const suffix = opts.suffixFn(index)
+  // opts.minSuffix: Use JS native formatting for smallish numbers, because
+  // '99,999' is prettier than '99.9k'
+  // TODO: handle 0 < val < 1 here
+  if (Math.abs(val) < opts.minSuffix) {
+    return Math.floor(val).toLocaleString()
+  }
+  // No suffix found: use scientific notation. JS's native toExponential is fine.
+  if (!suffix && suffix !== '') {
+    return val.toExponential(opts.sigfigs-1).replace('e+', 'e')
+  }
+  // Found a suffix. Calculate the prefix, the number before the suffix.
+  const prefix = (val / Math.pow(1000, index)).toPrecision(opts.sigfigs)
+  return `${prefix}${suffix}`
 }
 
 const defaultOptions = {
-  // TODO short suffixes
-  suffixes: suffixes.long,
+  suffixGroups: suffixes, 
+  // Flavor is a shortcut to modify any number of other options, like sigfigs.
+  // It's much more commonly used by callers than suffixGroup, which only controls
+  // suffixes. The two share the same possible names by default.
+  flavor: 'long',
+  suffixGroup: 'long',
   suffixFn(index) {
-    if (index <= this.suffixes.length) {
-      return this.suffixes[index] || ''
+    var suffixes = this.suffixes || this.suffixGroups[this.suffixGroup]
+    validate(suffixes, `no such suffixgroup: ${this.suffixGroup}`)
+    if (index < suffixes.length) {
+      return suffixes[index] || ''
     }
     // return undefined
   },
   // minimum value to use any suffix, because '99,900' is prettier than '99.9k'
   minSuffix: 1e5,
-  sigfigs: 3,
+  sigfigs: 3, // often overridden by flavor
   format: 'standard'
 }
-export class Formatter {
+// User-visible format choices, like on swarmsim's options screen. 
+// Each has a different set of options.
+const Formats = {
+  standard: {},
+  // like standard formatting, with no suffixes at all
+  scientific: {suffixGroups: {long: [], short: []}},
+  // like standard formatting, with a smaller set of suffixes
+  hybrid: {
+    suffixGroups: {
+      long: defaultOptions.suffixGroups.long.slice(0, 12),
+      short: defaultOptions.suffixGroups.short.slice(0, 12),
+    },
+  },
+  // like standard formatting, with a different/infinite set of suffixes
+  engineering: {suffixFn: index => index === 0 ? '' : `E${index*3}`},
+}
+// A convenient way for the developer to modify formatters.
+// These are different from formats - not user-visible.
+const Flavors = {
+  long: {suffixGroup: 'long', sigfigs: 5},
+  short: {suffixGroup: 'short', sigfigs: 3},
+}
+// Allow callers to extend formats and flavors.
+defaultOptions.formats = Formats
+defaultOptions.flavors = Flavors
+
+class Formatter {
   constructor(opts = {}) {
-    this.opts = Object.assign({}, defaultOptions, opts)
+    this.opts = opts
   }
   
+  _normalizeOpts(opts={}) {
+    // all the user-specified opts, no defaults
+    opts = Object.assign({}, this.opts, opts)
+    // opts.format redefines some other opts, but should never override the user's opts
+    var format = opts && opts.format
+    var formats = (opts && opts.formats) || defaultOptions.formats
+    var formatOptions = formats[format || defaultOptions.format]
+    validate(formatOptions, `no such format: ${format}`)
+    var flavor = opts && opts.flavor
+    var flavors = (opts && opts.flavors) || defaultOptions.flavors
+    var flavorOptions = flavors[flavor || defaultOptions.flavor]
+    validate(flavorOptions, `no such flavor: ${flavor}`)
+    // finally, add the implied options: defaults and format-derived
+    return Object.assign({}, defaultOptions, formatOptions, flavorOptions, opts)
+  }
+  index(val) {
+    return _index(val)
+  }
+  suffix(val, opts) {
+    opts = this._normalizeOpts(opts)
+    return opts.suffixFn(_index(val))
+  }
   format(val, opts) {
-    opts = opts ? _defaults({}, opts, this.opts) : this.opts
-    return Formats[opts.format](val, opts);
+    opts = this._normalizeOpts(opts)
+    return _format(val, opts)
   }
 }
 
 const numberformat = new Formatter()
+numberformat.defaultOptions = defaultOptions
 numberformat.Formatter = Formatter
-export default numberformat;
+export default numberformat
--- test/unit/workspace.js
+++ test/unit/workspace.js
@@ -12,8 +12,10 @@ describe('numberformat', () => {
     //expect(formatter.format(8/9)).to.equal('0.889')
     //expect(formatter.format(1e3)).to.equal('1 thousand')
     expect(formatter.format(1e3)).to.equal('1,000')
-    expect(formatter.format(1e6)).to.equal('1.00 million')
-    expect(formatter.format(1.1111e6)).to.equal('1.11 million')
+    expect(formatter.format(1e6)).to.equal('1.0000 million')
+    expect(formatter.format(1.1111e6)).to.equal('1.1111 million')
+    expect(formatter.format(1e21)).to.equal('1.0000 sextillion')
+    expect(formatter.format(1e36)).to.equal('1.0000 undecillion')
   })
   it('formats hybrid', () => {
     const formatter = new numberformat.Formatter({format: 'hybrid'})
@@ -22,9 +24,9 @@ describe('numberformat', () => {
     //expect(formatter.format(8/9)).to.equal('0.889')
     //expect(formatter.format(1e3)).to.equal('1 thousand')
     expect(formatter.format(1e3)).to.equal('1,000')
-    expect(formatter.format(1e6)).to.equal('1.00 million')
-    expect(formatter.format(1.1111e6)).to.equal('1.11 million')
-    // TODO differences
+    expect(formatter.format(1e6)).to.equal('1.0000 million')
+    expect(formatter.format(1.1111e6)).to.equal('1.1111 million')
+    expect(formatter.format(1e36)).to.equal('1.0000e36')
   })
   it('formats scientificE', () => {
     const formatter = new numberformat.Formatter({format: 'scientific'})
@@ -33,8 +35,9 @@ describe('numberformat', () => {
     //expect(formatter.format(8/9)).to.equal('0.889')
     //expect(formatter.format(1e3)).to.equal('1e3')
     expect(formatter.format(1e3)).to.equal('1,000')
-    expect(formatter.format(1e6)).to.equal('1.00e6')
-    expect(formatter.format(1.1111e6)).to.equal('1.11e6')
+    expect(formatter.format(1e6)).to.equal('1.0000e6')
+    expect(formatter.format(1.1111e6)).to.equal('1.1111e6')
+    expect(formatter.format(1e36)).to.equal('1.0000e36')
   })
   it('formats engineering', () => {
     const formatter = new numberformat.Formatter({format: 'engineering'})
@@ -43,16 +46,63 @@ describe('numberformat', () => {
     //expect(formatter.format(8/9)).to.equal('0.889')
     //expect(formatter.format(1e3)).to.equal('1E3')
     expect(formatter.format(1e3)).to.equal('1,000')
-    expect(formatter.format(1e6)).to.equal('1.00E6')
-    expect(formatter.format(1.1111e6)).to.equal('1.11E6')
-    expect(formatter.format(1e7)).to.equal('10.0E6')
-    expect(formatter.format(1.11e7)).to.equal('11.1E6')
-    expect(formatter.format(1e8)).to.equal('100E6')
+    expect(formatter.format(1e6)).to.equal('1.0000E6')
+    expect(formatter.format(1.1111e6)).to.equal('1.1111E6')
+    expect(formatter.format(1e7)).to.equal('10.000E6')
+    expect(formatter.format(1.1111e7)).to.equal('11.111E6')
+    expect(formatter.format(1e8)).to.equal('100.00E6')
   })
+  it('formats short numbers', () => {
+    const formatter = new numberformat.Formatter({flavor: 'short'})
+    expect(formatter.format(1)).not.to.equal(1)
+    expect(formatter.format(1)).to.equal('1')
+    expect(formatter.format(1e3)).to.equal('1,000')
+    expect(formatter.format(1e6)).to.equal('1.00M')
+    expect(formatter.format(100e3)).to.equal('100K')
+    expect(formatter.format(1e9)).to.equal('1.00B')
+    expect(formatter.format(1.1111e6)).to.equal('1.11M')
+    expect(formatter.format(1.1111e6, {format: 'engineering'})).to.equal('1.11E6')
+  })
+  it('supports `opts` as a second argument', () => {
+    const formatter = numberformat
+    expect(formatter.format(1, {flavor: 'short'})).to.equal('1')
+    expect(formatter.format(1e3, {flavor: 'short'})).to.equal('1,000')
+    expect(formatter.format(1e6, {flavor: 'short'})).to.equal('1.00M')
+    expect(formatter.format(100e3, {flavor: 'short'})).to.equal('100K')
+    expect(formatter.format(1e9, {flavor: 'short'})).to.equal('1.00B')
+    expect(formatter.format(1.1111e6, {flavor: 'short'})).to.equal('1.11M')
+  })
+  it('handles bogus formats', () => {
+    const formatter = numberformat
+    expect(() => formatter.format(1, {format: 'bogus'})).to.throw()
+    expect(() => formatter.format(1, {format: 'standard'})).not.to.throw()
+    expect(() => formatter.format(1, {format: null})).not.to.throw()
+  })
+  it('supports suffix-only', () => {
+    const formatter = numberformat
+    expect(formatter.suffix(1e6, {format: 'standard'})).to.equal(' million')
+    expect(formatter.suffix(1e6, {format: 'standard', flavor: 'short'})).to.equal('M')
+    expect(formatter.suffix(1e6, {format: 'engineering'})).to.equal('E6')
+  })
+  it('supports negatives', () => {
+    const formatter = numberformat
+    expect(formatter.format(0)).to.equal('0')
+    expect(formatter.format(-1)).to.equal('-1')
+    expect(formatter.format(-1e3)).to.equal('-1,000')
+    expect(formatter.format(-1e6)).to.equal('-1.0000 million')
+    expect(formatter.format(-1e21)).to.equal('-1.0000 sextillion')
+  })
+  // TODO: rounding
+  // TODO: decimal support, 0 < val < 1
+  // TODO: negatives tests
 
   it('formats bigger numbers', () => {
     const formatter = numberformat
-    expect(formatter.format(1e18)).to.equal('1.00 quintillion')
-    expect(formatter.format(1e21)).to.equal('1.00 sextillion')
+    expect(formatter.format(1e18)).to.equal('1.0000 quintillion')
+    expect(formatter.format(1e18, {flavor: 'short'})).to.equal('1.00Qi')
+    // 1e20 is an important breakpoint: JS's native number formatting changes
+    // from '123,456' to '1.23e21'
+    expect(formatter.format(1e21)).to.equal('1.0000 sextillion')
+    expect(formatter.format(1e21, {flavor: 'short'})).to.equal('1.00Sx')
   })
 });
