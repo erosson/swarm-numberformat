2f7e3cf92bbc0a793e14ee333a84592e66696c65
Evan <github@erosson.org>
2017-05-30 15:56:59 -0700

    more import shenanigans. allow both config and global decimal.

--- src/decimal.js
+++ src/decimal.js
@@ -1,20 +1,15 @@
-function log(...mess) {
-  //console.log(...mess)
-}
 // Lazy-load - we might not need decimal. It's a peerDependency, so the parent
 // library must include it if needed - we don't, because many callers don't need
 // it.
 let Decimal
 export function requireDecimal(config) {
-  return Decimal || (Decimal = (function() {
+  return (config && config.Decimal) || Decimal || (Decimal = (function() {
     // Allow node callers to inject their own decimal.js, because I am sick to
     // death of trying to wrangle require/webpack/import/etc.
-    if (config && config.Decimal) {
-      log('swarm-numberformat decimal.js: Found config.Decimal')
-      return config.Decimal
+    if (global && global.Decimal) {
+      return global.Decimal
     }
     // `nwb.config.js: extenals` ensures this points to window.Decimal for umd (`<script src="...">`) builds
-    log('swarm-numberformat decimal.js: trying require()')
     return require('decimal.js')
   })())
 }
